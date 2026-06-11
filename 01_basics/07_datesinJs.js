4953f6bab77fa0299348ae8510e5015c9b4e3550
aizedx18 <aizedkhan780@gmail.com>
2026-06-11 10:19:15 +0000

    Dates in Js

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,9 @@
+// Date 
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
